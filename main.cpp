e32aa24e6328d3bfd594ae5710a82661b5f5b9ca
pipinkurniawati <13513089@std.stei.itb.ac.id>
2016-03-20 17:11:13 +0700

    konflik kzl

--- main.cpp
+++ main.cpp
@@ -702,12 +702,11 @@ void placePin() {
 	drawPin(278,320); // Jogja
 	drawPin(340,312); // Surabaya
 
-<<<<<<< HEAD
 	//PAPUA
 	drawPin(480,187); //irian barat
 	drawPin(590,222); //irian timur
 	drawPin(547,255); //irian tengah
-=======
+
 	// KALIMANTAN
 	drawPin(260,130); // Kalimantan timur
 	drawPin(245,200); // Kalimantan selatan
@@ -730,5 +729,4 @@ void placeProvinceName(string str, int x, int y) {
 	else if(str.compare("jawa")==0) {
 
 	}
->>>>>>> a46e2dd25170eb83f20daec42b60331ff509c187
 }
